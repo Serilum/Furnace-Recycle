3ac38646cac758115c236903c8a4178e686b77bc
Rick South <rick@natamus.com>
2024-07-28 07:31:58 +0200

    (v2.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/furnacerecycle/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/furnacerecycle/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.furnacerecycle;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.furnacerecycle.util.Reference;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
@@ -12,6 +13,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
