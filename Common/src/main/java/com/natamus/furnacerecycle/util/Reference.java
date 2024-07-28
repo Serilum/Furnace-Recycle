c5b4f838d1de56ce6a5c1031bbd664f27cab9846
Rick South <rick@natamus.com>
2024-07-28 07:31:54 +0200

    (v2.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/furnacerecycle/util/Reference.java
+++ Common/src/main/java/com/natamus/furnacerecycle/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.furnacerecycle.util;
 public class Reference {
 	public static final String MOD_ID = "furnacerecycle";
 	public static final String NAME = "Furnace Recycle";
-	public static final String VERSION = "2.2";
+	public static final String VERSION = "2.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
