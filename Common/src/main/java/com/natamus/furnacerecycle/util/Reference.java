e8ae9ac54f149621eca021360c3aed04f9369dc0
Rick South <rick@natamus.com>
2026-06-18 23:52:15 +0200

    Updated Furnace Recycle to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/furnacerecycle/util/Reference.java
+++ Common/src/main/java/com/natamus/furnacerecycle/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "furnacerecycle";
 	public static final String NAME = "Furnace Recycle";
 	public static final String VERSION = "2.6";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
